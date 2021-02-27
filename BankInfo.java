ddbcf2654f2b62c8448bd71f7ba01f5ee4a9d16b
NaveenaAshok <79351622+NaveenaAshok@users.noreply.github.com>
2021-02-28 01:42:24 +0530

    Delete BankInfo.java

--- BankInfo.java
+++
@@ -1,7 +0,0 @@
-package org.student;
-
-public class BankInfo {
-	public void saving() {System.out.println("this is saving method");} 
-	public void fixed() {System.out.println("You are inside fixed method");} 
-	public void deposit() {System.out.println("You are inside deposit method of Bank Info");}
-}
